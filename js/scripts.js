a2e577c019c3ba9fd742450e3504db848a83cda2
Julianna Gray <julianna.a.gray@gmail.com>
2018-03-11 15:22:43 -0400

    added javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Welcome To My Portfolio!!!")
+
+$(document).ready(function() {
+  $(".project").on("mouseenter", function() {
+    $(this).find(".project-details").fadeIn();
+  });
+  $(".project").on("mouseleave", function() {
+    $(this).find(".project-details").fadeOut();
+  });
+});
